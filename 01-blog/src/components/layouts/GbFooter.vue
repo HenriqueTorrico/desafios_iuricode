4b1578efc2a49b579fe0182df2641fec25fbee74
HenriqueTorrico <henriquetorrico12@gmail.com>
2023-10-04 13:30:37 -0300

    Feat: created and style component layout Footer

---
+++ 01-blog/src/components/layouts/GbFooter.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer>
+    <h6>
+      Desenvolvido por <a href="https://www.torricodev.net.br" target="_blank">Henrique Torrico</a>
+    </h6>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem 0;
+  background: $gradient-brand-color;
+
+  a,
+  h6 {
+    color: $dark-40;
+  }
+
+  h6 {
+    font: $weight-400 $size-small $family-primary;
+    line-height: 1.25rem;
+  }
+}
+</style>
